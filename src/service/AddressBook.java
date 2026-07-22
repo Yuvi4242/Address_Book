1fd91d7463d6952d725a6cb7f580f8552b42e2a0
Yuvraj Singh <yuvrajsingh43344@gmail.com>
2026-07-22 13:21:10 +0530

    [yuvraj]Add:<UC3 done>

---
+++ src/service/AddressBook.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.Contact;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+    private List<Contact> contactList;
+
+    public AddressBook() {
+        this.contactList = new ArrayList<>();
+    }
+
+    public void addContact(Contact contact) {
+        contactList.add(contact);
+    }
+
+    public List<Contact> getContactList() {
+        return contactList;
+    }
+
+    public Contact findContactByFirstName(String firstName) {
+        for (Contact contact : contactList) {
+            if (contact.getFirstName().equalsIgnoreCase(firstName)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+}
